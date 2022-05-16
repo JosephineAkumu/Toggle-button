9161b74ed56f1bf5ffb34669d0f12ed1fac150ff
Josephine     Akumu <44413287+JosephineAkumu@users.noreply.github.com>
2022-05-16 16:29:54 +0300

    Add files via upload

---
+++ Dark Mode toggle/main.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "pink";
+  } else {
+    bodyEl.style.background = "blue";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
